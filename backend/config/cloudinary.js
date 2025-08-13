ad8943a38565908ff655a4d7d9101200c4840fdf
RudrakshTaya <rudrakshtaya8777.com>
2025-08-13 12:40:05 +0530

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,13 +1,15 @@
 import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
-
+console.log("cloud",process.env.CLOUDINARY_CLOUD_NAME)
 // Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  
 });
+
 
 // Configure Cloudinary storage for multer
 const storage = new CloudinaryStorage({
